9ff02d031e93a2002881453c4abd1416f8692405
Yashkamble21-ysk <kyash5384@gmail.com>
2025-10-05 17:25:19 +0530

    commit

--- health-hub-backend/src/main/java/com/hubhealth/health_hub_backend/repository/DoctorAndPatientRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hubhealth.health_hub_backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.hubhealth.health_hub_backend.entity.DoctorAndPatient;
-
-@Repository
-public interface DoctorAndPatientRepository extends JpaRepository<DoctorAndPatient, Long> {
-}
